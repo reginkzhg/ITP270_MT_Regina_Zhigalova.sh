682405062fe362a0f0268ef2a63ce51496134ae5
Cyber Range Student <student@ip-10-1-55-35.ec2.internal>
2023-04-17 13:30:32 +0000

    My If

---
+++ If_else_exercise/if_else_exercise.sh
@@ -0,0 +1,27 @@
+#!/bin/python
+
+print (Here is some information about planets:\n)
+print (" 1).Venus 2)Mars 3)Jupiter ")
+print (" 4)Saturn 5)Uranus 6)Neptune\n")
+
+
+weight = 185
+	planet=int(input("Enter the number for planet:))
+
+if planet==1
+	weight *=0.91
+	print ("On that planet your weigth woulde be" weight)
+else
+if planet ==2 
+	weight *=0.38
+	print ("On that planet your weight woulde be" weight)
+else
+if planet==3
+	weight *=2.34
+	print ("On that planet your weight woulde be" weight)
+
+else
+if planet==4
+	weigth *=1.06
+	print ("On that planet your weight woulde be" weight)
+fi
